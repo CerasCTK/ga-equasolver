a5cf59ddbd9c136108002c8d852232dd86bf22f0
Do Thanh Tung <dothanhtungnb602@gmail.com>
2026-07-23 15:30:31 +0700

    Update lexer, parser and add evaluate function

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { Lexer } from './lexer.js'
 import { Parser } from "./parser.js"
 
 document.querySelector('#app').innerHTML = `
-<input id="equation" type="text">
+<input id="equation" type="text" value="2x + y = 10">
 <button id="tokenize" type="button">Tokenize</button>
 `
 
@@ -14,6 +14,20 @@ button.addEventListener("click", () => {
     const tokens = lexer.tokenize();
     console.table(tokens);
     const parser = new Parser(tokens);
-    const ast = parser.parse();
-    console.log(JSON.stringify(ast, null, 4));
+    parser.parse();
+    console.log(JSON.stringify(parser.ast, null, 4));
+	console.log(parser.variables);
+	const population1 = new Map();
+	population1.set("x", 2);
+	population1.set("y", 6);
+
+	const population2 = new Map();
+	population2.set("x", 2);
+	population2.set("y", 8);
+
+	console.log(parser.evaluate(population1));
+	console.log(parser.evaluate(population2));
 });
+
+// 2xyz + 3x^2y - 4xy^2 = 5(x + y)(z - 2)
+// -2x^3(x - 1)^2 + 3(x + 2)(x^2 - 4x + 5) = (x - 1)(2x + 3)^2 - x/2
--- src/parser.js
+++ src/parser.js
@@ -5,6 +5,8 @@ export class Parser{
     constructor(tokens){
         this.tokens = tokens;
         this.currentIndex = 0;
+		this.variables = new Set();
+		this.ast = {};
     }
 
     peek(){
@@ -33,11 +35,6 @@ export class Parser{
     }
 
 	parse(){
-		const equation = this.parseEquation();
-		return equation;
-	}
-
-	parseEquation(){
 		const lhs = this.parseExpression();
 
 		if(!this.match(TokenType.EQUAL)){
@@ -47,7 +44,7 @@ export class Parser{
             this.advance();
 		}
 		const rhs = this.parseExpression();
-		return new BinaryExpr(Operator.EQUAL, lhs, rhs);
+		this.ast = new BinaryExpr(Operator.EQUAL, lhs, rhs);
 	}
 
 	parseExpression(){
@@ -116,6 +113,7 @@ export class Parser{
 		}
 		if(this.match(TokenType.VARIABLE)){
             this.advance();
+			this.variables.add(this.previous().value);
 			return new VariableExpr(this.previous().value);
 		}
 		if(this.match(TokenType.OPEN_PARENTHESIS)){
@@ -129,5 +127,49 @@ export class Parser{
             return expression;
 		}
 		throw new Error("Parse factor error");
+	}
+
+	evaluate(variablesMap){
+		if(!(this.ast instanceof BinaryExpr) || this.ast.op !== Operator.EQUAL){
+			throw new Error("Invalid AST");
+		}
+		const lhs = this.recursion(this.ast.lhs, variablesMap);
+		const rhs = this.recursion(this.ast.rhs, variablesMap);
+		return lhs === rhs;
+	}
+
+	recursion(node, variablesMap){
+		if(node instanceof NumberExpr){
+			return node.value;
+		}
+		if(node instanceof VariableExpr){
+			return variablesMap.get(node.name);
+		}
+		if(node instanceof UnaryExpr){
+			const value = this.recursion(node.operand, variablesMap);
+			return node.op === Unary.POSITIVE ? value : -value;
+		}
+		if(node instanceof BinaryExpr){
+			const lhs = this.recursion(node.lhs, variablesMap);
+			const rhs = this.recursion(node.rhs, variablesMap);
+			switch(node.op){
+				case Operator.ADD:
+					return lhs + rhs;
+				case Operator.SUBTRACT:
+					return lhs - rhs;
+				case Operator.MULTIPLY:
+					return lhs * rhs;
+				case Operator.DIVIDE:
+					if(rhs === 0){
+						throw new Error("divide by zero");
+					}
+					return lhs / rhs;
+				case Operator.POWER:
+					return lhs ** rhs;
+				default:
+					throw new Error("This should not be happened!");
+			}
+		}
+		throw new Error("This should not be happened!");
 	}
 }
